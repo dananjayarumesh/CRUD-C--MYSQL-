62d40d23e1fbffbb8196ff8b8652042bd6abb7fd
rumeshdananjaya <rumeshpriv@gmail.com>
2018-05-14 23:20:24 +0530

    initial commit

---
+++ MySqlCRUD/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MySqlCRUD
+{
+    class Connection
+    {
+        public MySqlConnection dbConnect()
+        {
+            //String connect = "server=localhost;Username=root;Password=,Database=ccrud";
+            string connect = "Server=localhost; Port=3306; Database=ccrud; Uid=root; Pwd=;SslMode=none;";
+            MySqlConnection conn = new MySqlConnection(connect);
+            return conn;
+        }
+    }
+}
---
+++ MySqlCRUD/Form1.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySqlCRUD
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readData();
+        }
+
+        Connection connect = new Connection();
+        MySqlDataAdapter dataAdapter;
+        DataTable dataTable;
+
+        public void readData()
+        {
+            MySqlConnection conn = connect.dbConnect();
+            String query = "SELECT * From crud_table";
+            dataTable = new DataTable();
+            dataAdapter = new MySqlDataAdapter(query, conn);
+            dataAdapter.Fill(dataTable);
+            gridData.DataSource = dataTable;
+        }
+    }
+}
